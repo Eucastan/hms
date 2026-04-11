99f56f238a2732aaba2bb1c2e085c2b582b981fe
Stanley Emeh <stanemeh808@gmail.com>
2026-04-11 12:29:07 +0100

    API Gateway configuration file

---
+++ api-gateway/configs/config.go
@@ -0,0 +1,36 @@
+package config
+
+import (
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	Port               string
+	JWTSecret          string
+	AuthServiceURL     string
+	PatientServiceURL  string
+	LabServiceURL      string
+	PharmacyServiceURL string
+	BillingServiceURL  string
+	ClinicalServiceURL string
+	ServiceName        string
+	Version            string
+}
+
+func Load() (*Config, error) {
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(".")
+	viper.AutomaticEnv()
+
+	if err := viper.ReadInConfig(); err != nil {
+		return nil, err
+	}
+
+	var cfg Config
+	if err := viper.Unmarshal(&cfg); err != nil {
+		return nil, err
+	}
+
+	return &cfg, nil
+}
